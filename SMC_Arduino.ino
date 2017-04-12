98b2cc66a00ff9e3c921b6b6b4ba1cc3ba40835b
zq9120 <zq9120@yeah.net>
2017-04-12 16:34:23 +0800

    update

--- SMC_Arduino.ino
+++ SMC_Arduino.ino
@@ -1,10 +1,30 @@
 #include <Stepper.h>
 #include <SoftwareSerial.h>
+
+// PIN DEFINE START
+#define LIGHT_GATE      2
+#define YD_WRITE_ENABLE 3
+#define YD_VCC          4
+
+#define TRIGGER         5
+
+#define SWITCHA         6
+#define SWITCHB         7
+
+#define L298N_IN1       8
+#define L298N_IN2       9
+#define L298N_IN3       10
+#define L298N_IN4       11
+
+#define SERIAL_RX       12
+#define SERIAL_TX       13
+// PIN DEFINE END
+
 #define BYTE            unsigned char
 #define YD_STATUS_RUN   0
 #define YD_STATUS_STOP  1
 #define YD_STATUS_HOLD  2
-#define LIGHT_GATE      2
+
 #define MOTOR_STEP      200
 #define MOTOR_HEIGHT    50
 
@@ -14,9 +34,14 @@
 #define PT_MOTOR  0x00
 #define PT_THERM  0x01
 #define PT_DELAY  0x02
+#define PT_SWITCHA  0x03
+#define PT_SWITCHB  0x04
+#define PT_TRIGGER  0x05
 
 #define PT_MOTOR_UP   0x00
 #define PT_MOTOR_DOWN 0x01
+#define PT_SWITCH_CUT  0x00
+#define PT_SWITCH_CLOSURE 0x01
 
 struct PromaItem {
   PromaType promaType;
@@ -30,12 +55,22 @@ void setYdTemperature(int t);
 void setYdRate(int t);
 void setYdStatus(int status);
 
-Stepper stepper(MOTOR_STEP, 8, 9, 10, 11);
-SoftwareSerial ydSerial(12, 13);
+Stepper stepper(MOTOR_STEP, L298N_IN1, L298N_IN2, L298N_IN3, L298N_IN4);
+SoftwareSerial ydSerial(SERIAL_RX, SERIAL_TX);
 PromaItem item;
+bool runningFlag = false;
 
 void setup() {
   pinMode(LIGHT_GATE, INPUT);
+  pinMode(YD_VCC, OUTPUT);
+  pinMode(TRIGGER, INPUT);
+  pinMode(YD_WRITE_ENABLE, OUTPUT);
+  pinMode(SWITCHA, OUTPUT);
+  pinMode(SWITCHB, OUTPUT);
+  digitalWrite(YD_VCC, HIGH);
+  digitalWrite(YD_WRITE_ENABLE, HIGH);
+  digitalWrite(SWITCHA, LOW);
+  digitalWrite(SWITCHB, LOW);
   Serial.begin(9600);
   Serial.println(sizeof(PromaItem));
   ydSerial.begin(9600);
@@ -45,6 +80,20 @@ void setup() {
 }
 
 void loop() {
+  if(digitalRead(TRIGGER) == LOW && !runningFlag) {
+    delay(10);
+    if(digitalRead(TRIGGER) == LOW && !runningFlag) {
+      runningFlag = true;
+      Serial.write(0x42);
+      Serial.write(0x4d);
+    }
+  }
+  if(digitalRead(TRIGGER) == HIGH && runningFlag) {
+    delay(10);
+    if(digitalRead(TRIGGER) == HIGH && runningFlag) {
+      runningFlag = false;
+    }
+  }
   if(Serial.available() > 0) {
     Serial.readBytes((char*) &item, sizeof(PromaItem));
     Serial.write((char*) &item, sizeof(PromaItem));
@@ -57,6 +106,12 @@ void loop() {
       setYdTemperature(item.wParam);
       setYdRate(item.wParam);
       setYdStatus(YD_STATUS_RUN);
+      break;
+    case PT_SWITCHA:
+      digitalWrite(SWITCHA, item.wParam == PT_SWITCH_CLOSURE ? HIGH : LOW);
+      break;
+    case PT_SWITCHB:
+      digitalWrite(SWITCHB, item.wParam == PT_SWITCH_CLOSURE ? HIGH : LOW);
       break;
     default:
       break;
